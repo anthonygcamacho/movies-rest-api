3075097fd32a26f3cc5fddf7e7cc0170e66bf512
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-02-05 11:03:25 -0800

    building

--- build/routes/api/v1/actors.js
+++ build/routes/api/v1/actors.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const getActors_controller_1 = require("../../../controllers/api/v1/getActors.controller");
 const getActorsByID_controller_1 = require("../../../controllers/api/v1/getActorsByID.controller");
-const router = express_1.default.Router();
 // -------------------------------------------------------------------------------
+const router = express_1.default.Router();
 router.get("/", getActors_controller_1.getActorsController);
 router.get("/:actor_id", getActorsByID_controller_1.getActorByIDController);
 exports.default = router;
--- src/routes/api/v1/actors.ts
+++ src/routes/api/v1/actors.ts
@@ -2,9 +2,9 @@ import express from "express"
 import { getActorsController } from "../../../controllers/api/v1/getActors.controller"
 import { getActorByIDController } from "../../../controllers/api/v1/getActorsByID.controller"
 
-const router = express.Router()
-
 // -------------------------------------------------------------------------------
+
+const router = express.Router()
 
 router.get("/", getActorsController)
 router.get("/:actor_id", getActorByIDController)
