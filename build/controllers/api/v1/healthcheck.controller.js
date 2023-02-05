dd56705dc0bbed12f38502efc4c102458514ae4f
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-02-05 10:59:21 -0800

    building

--- build/controllers/api/v1/healthcheck.controller.js
+++ build/controllers/api/v1/healthcheck.controller.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.healthCheckController = void 0;
 const healthCheckController = (req, res) => {
-    res.status(200).json({ status: 'UP' });
+    res.status(200).json({ status: "UP" });
 };
 exports.healthCheckController = healthCheckController;
--- src/controllers/api/v1/healthcheck.controller.ts
+++ src/controllers/api/v1/healthcheck.controller.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express"
+import { RequestHandler } from "express"
 
-export const healthCheckController = (req: Request, res: Response):void => {
-    res.status(200).json({status: 'UP'})
+export const healthCheckController: RequestHandler = (req, res): void => {
+    res.status(200).json({ status: "UP" })
 }
