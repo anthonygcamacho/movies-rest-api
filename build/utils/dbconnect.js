63f799d09db4fdaefe017c1acfdd44d864099e7b
anthonygcamacho <anthony.g.camacho@gmail.com>
2023-02-05 10:53:39 -0800

    building

--- build/utils/dbconnect.js
+++ build/utils/dbconnect.js
@@ -2,18 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 require("dotenv").config();
-// const { Pool } = require("pg")
-// const pool = new Pool()
-// const databaseUrl = process.env.DATABASE_URL
-// const pool = new Pool({
-//     connectionString: databaseUrl,
-// })
-// module.exports = {
-//     query: (text, params) => pool.query(text, params),
-// }
-const initOptions = {
-/* initialization options */
-};
+const initOptions = {};
 const pgp = require("pg-promise")(initOptions);
 const cn = process.env.DATABASE_URL;
 exports.db = pgp(cn);
--- src/utils/dbconnect.ts
+++ src/utils/dbconnect.ts
@@ -1,18 +1,6 @@
 require("dotenv").config()
 
-// const { Pool } = require("pg")
-// const pool = new Pool()
-// const databaseUrl = process.env.DATABASE_URL
-// const pool = new Pool({
-//     connectionString: databaseUrl,
-// })
-// module.exports = {
-//     query: (text, params) => pool.query(text, params),
-// }
-
-const initOptions = {
-    /* initialization options */
-}
+const initOptions = {}
 const pgp = require("pg-promise")(initOptions)
 
 const cn = process.env.DATABASE_URL
